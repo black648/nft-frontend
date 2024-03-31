68337058a9aa3ed660174ae505e9b1009bb99578
HklMini <bastlwalzer@gmail.com>
2024-03-31 23:40:20 +0900

    Create 기능 구현

--- src/components/TodoFormModal.tsx
+++ src/components/TodoFormModal.tsx
@@ -1,29 +1,57 @@
 import styles from "../../styles/todo.module.css";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store/store";
-import Image from "next/image";
-import CloseImage from "../../public/icon_close.png"
 import {displayModal} from "../store/modalSlice";
+import CloseImage from "../../public/icon_close.png";
+import Image from "next/image";
+import React, {useState} from "react";
+import {createTodo} from "../store/todoSlice";
 
 const TodoFormModal = () => {
     const modal = useSelector((state: RootState) => state.modalSlice.display);
+    const todos = useSelector((state: RootState) => state.todoSlice.todos);
     const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+
     const displayModalHandler = () => {
         dispatch(displayModal(false));
     };
+
+    const submitHandle = (e: React.FormEvent) => {
+        e.preventDefault();
+        const now = new Date().toISOString().slice(0, 10);
+        dispatch(createTodo({
+            title,
+            content,
+            createdAt: now
+        }));
+
+        // 초기화
+        setTitle('');
+        setContent('');
+
+        // 모달 창 닫기
+        dispatch(displayModal(false));
+    }
 
     return (
         <>
             {
                 modal && <div className={styles.popup}>
-                    <div className={styles.popupBg}></div>
+                    <div className={styles.popupBg} />
                     <div className={styles.popCont}>
-                        <input type="text" placeholder="제목을 입력하세요" className={styles.popTitle} />
-                        <textarea placeholder="내용을 입력하세요" className={styles.popText}></textarea>
-
-                        <div className={styles.buttonGroup}>
-                            <button type="button" className={styles.popBtn} onClick={() => displayModalHandler()}>닫기</button>
-                        </div>
+                        <button className={styles.closeBtn}>
+                            <Image src={CloseImage} alt={"닫기"} onClick={() => displayModalHandler()} />
+                        </button>
+                        <form onSubmit={submitHandle}>
+                            <input type="text" placeholder="제목을 입력하세요" className={styles.popTitle}  onChange={(e) => setTitle(e.target.value)} />
+                            <textarea placeholder="내용을 입력하세요" className={styles.popText}  onChange={(e) => setContent(e.target.value)}></textarea>
+                            <div className={styles.buttonGroup}>
+                                <button type="submit" className={styles.popBtn}>등록</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             }
